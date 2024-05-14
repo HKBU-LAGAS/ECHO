a64c585056d1da89cf518dee981a16b1d57c6f3c
Anry Yang <AnryYang@users.noreply.github.com>
2024-05-14 17:23:13 +0800

    Update run_n2v.sh

--- src/run_n2v.sh
+++ src/run_n2v.sh
@@ -3,5 +3,5 @@ python node2vec_main.py --data $1 --EC BDRC
 python node2vec_main.py --data $1 --EC EB
 python node2vec_main.py --data $1 --EC EP
 python node2vec_main.py --data $1 --EC EK
-python node2vec_main.py --data $1 --EC ERK
+python node2vec_main.py --data $1 --EC ECHO
 python node2vec_main.py --data $1 --EC GTOM
